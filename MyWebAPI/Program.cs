e7e2b181e0b66ff1f4f0a3c6de73df71e89d6e09
Gayathri Mahadevan <35684683+gayat19@users.noreply.github.com>
2026-03-23 09:33:43 +0530

    User Change Mimicd

--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -13,7 +13,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+Console.WriteLine("CHanged");
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
